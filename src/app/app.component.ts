10541098f3c5fb6f5129ec1cb1642fc540c4b662
W8inG4DeatH <lipinski.m@o2.pl>
2022-06-13 14:52:34 +0200

    menu and content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [
+    './app.component.scss',
+    './../assets/scss/colors.scss',
+    './../assets/scss/buttons.scss'
+  ]
 })
+
 export class AppComponent {
   title = 'comix';
 }
