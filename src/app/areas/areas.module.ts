6a026b0f9290c6f880e298b3d4b5e01c35f7f185
W8inG4DeatH <lipinski.m@o2.pl>
2022-06-17 16:00:07 +0200

    main header

--- src/app/areas/areas.module.ts
+++ src/app/areas/areas.module.ts
@@ -5,9 +5,12 @@ import { DashboardModule } from './dashboard/dashboard.module';
 import { ListModule } from './list/list.module';
 import { EditionModule } from './edition/edition.module';
 import { LoginingModule } from './logining/logining.module';
+import { MainHeaderComponent } from './main-header/main-header.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [
+    MainHeaderComponent
+  ],
     imports: [
         CommonModule,
         DashboardModule,
